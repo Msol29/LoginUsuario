4f29750a33e69473c86bc86d6295e20fff80b5af
Msol29 <msolob@hotmail.com>
2022-07-19 11:47:41 -0300

    Agregar archivos de proyecto.

---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Dado el siguiente codigo:
+// string usuario= "Pepito", clave "1234";
+//Informar los siguientes casos:
+// Si usuario = "pepito" y clave "1234" informar Bienvenido pepito!"
+// Si usuario = "pepito" y clave no es "1234" informar "Clave Incorrecta"
+// Si usuario = no es "pepito" informar "usuario no existe"
+
+string usuario="pepito", clave="1234";
+
+if (usuario == "pepito")
+{ 
+    if (clave == "123456")
+
+        Console.WriteLine("Bienvenido Pepito!");
+    else
+        Console.WriteLine("Clave Incorrecta");
+}
+else
+    Console.WriteLine ("Usuario no existe");
